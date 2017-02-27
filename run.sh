3c78ee725e8834015fce9da3ce75165106604d47
Artem Kolomycev <artyhedgehog@gmail.com>
2017-02-28 00:22:47 +0300

    Added some tools and apps

--- run.sh
+++ run.sh
@@ -12,7 +12,7 @@ sudo apt-get update
 #######################
 # Installing software #
 #######################
-sudo apt-get install -y git htop emacs whois zsh clipit mc ipython shutter unrar shntool cuetools flac cmake checkinstall
+sudo apt-get install -y git htop emacs whois zsh clipit mc ipython shutter unrar shntool cuetools flac cmake checkinstall openvpn suckless-tools nfs-kernel-server iotop curl
 sudo apt-get install -y yakuake calibre gparted baobab clementine vlc krusader kdiff3 
 sudo apt-get install -y adobe-flashplugin
 sudo apt-get install -y oracle-java8-installer oracle-java8-set-default
@@ -20,6 +20,10 @@ sudo apt-get install -y android-tools-adb
 sudo apt-get install -y wine1.8-amd64 wine1.8 winetricks
 sudo apt-get install -y mysql-server-5.5 php5 php5-cli php5-intl php5-mysql php5-memcache php5-imagick php5-gd php5-dev memcached php5-apcu php5-sqlite php5-pgsql php5-xdebug php5-readline
 sudo apt-get install evolvere-icon-suite -y
+
+sudo apt-get install -y python-pip build-essential python-dev
+sudo pip install Glances
+sudo pip install PySensors
 
 # http://keepass.info
 # sudo apt-get install -y keepass2
@@ -42,6 +46,3 @@ sudo apt-get -s install -y packages/*.deb
 ###########################
 # Raw added lines follow: #
 ###########################
-sudo apt-get install python-pip build-essential python-dev
-sudo pip install Glances
-sudo pip install PySensors
